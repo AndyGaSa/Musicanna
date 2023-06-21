9b9c911b3a48be420a8f79b8223f973784cb8b53
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2023-06-21 15:03:15 +0200

    Add mediaqueries to header to show a navbar or a burger menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,7 +20,34 @@ const Header = () => {
             <Image width={80} height={80} src={logoDark} alt="logoDark" />
           </div>
         </Link>
-        <div>
+        <div className="hidden md:flex flex-1 justify-center">
+          <ul className="flex space-x-14 text-lg font-semibold">
+            <li>
+              <Link href="/">Inici</Link>
+            </li>
+            <li>
+              <Link href="/categories/cancons">Cançons</Link>
+            </li>
+            <li>
+              <Link href="/categories/contes verticals">Contes Verticals</Link>
+            </li>
+            <li>
+              <Link href="/categories/comptacontes">Comptacontes</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contacte</Link>
+            </li>
+            <li
+              className="cursor-pointer"
+              onClick={() => {
+                router.push('/', '/', { locale: 'es' });
+              }}
+            >
+              ES
+            </li>
+          </ul>
+        </div>
+        <div className="md:hidden">
           <button
             className="text-lg"
             onClick={toggleMenu}
@@ -31,7 +58,7 @@ const Header = () => {
         </div>
       </div>
       {isMenuOpen && (
-        <div className="max-w-7xl mx-auto px-4 pb-8 text-white bg-black bg-opacity-50">
+        <div className="max-w-7xl mx-auto px-4 pb-8 text-white bg-black bg-opacity-50 md:hidden">
           <ul className="space-y-4 text-lg font-semibold">
             <li>
               <Link href="/">Inici</Link>
