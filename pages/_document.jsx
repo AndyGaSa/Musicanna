087fec115e2d84049e58fb2bdea24795dd830831
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2023-08-09 21:16:14 +0200

    manifest test

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,7 +8,6 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         ></link>
-        <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
         <Main />
