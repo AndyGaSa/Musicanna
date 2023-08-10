b10449e841e9f677e50a6ed8d83f6f451aa3de4d
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2023-08-11 01:54:53 +0200

    update banner bottom test

--- __tests__/BannerBottom.test.jsx
+++ __tests__/BannerBottom.test.jsx
@@ -1,21 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import BannerBottom from '../components/BannerBottom';
 
+const mockBannerBottomProps = {
+  homeText: [
+    {
+      title: 'Musicanna',
+      subtitle:
+        'Cançoncs, contes verticals i contacontes per a nens i nenes de 0 a 6 anys',
+    },
+  ],
+};
+
 describe('BannerBottom', () => {
   test('renders the banner bottom component', () => {
-    render(<BannerBottom />);
+    render(<BannerBottom {...mockBannerBottomProps} />);
 
     // Assert
     const titleElement = screen.getByText(/musicanna/i);
     const subtitleElement = screen.getByText(
       /cançoncs, contes verticals i contacontes per a nens i nenes de 0 a 6 anys/i
     );
-    const descriptionElement = screen.getByText(
-      /un món d'imaginació, creativitat, contes i cançons/i
-    );
 
     expect(titleElement).toBeInTheDocument();
     expect(subtitleElement).toBeInTheDocument();
-    expect(descriptionElement).toBeInTheDocument();
   });
 });
