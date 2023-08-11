6228cb52ad7267c9ffe5c1846a2b3bc0667583c0
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2023-08-11 16:39:34 +0200

    add scenarios to index test

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -85,4 +85,52 @@ describe('Home', () => {
       expect(getByText(homeText.subtitle)).toBeInTheDocument();
     });
   });
+
+  it('renders the home page correctly when there are no posts', () => {
+    // Mock the props for the Home component
+    const props = {
+      posts: [],
+      bannerImages: [
+        {
+          _id: '1',
+          image: {
+            asset: { _ref: 'image-Tb9Ew8CXIwaY6R1kjMvI0uRR-2000x3000-jpg' },
+          },
+        },
+      ],
+      headerProps: {
+        categories: [
+          { title: 'Category 1', subtitle: 'Categories Subtitle 1' },
+        ],
+        contact: [{ title: 'Contact 1', subtitle: 'Contact Subtitle 1' }],
+      },
+      bannerBottomProps: {
+        homeText: [{ title: 'Home Text 1', subtitle: 'index Subtitle 1' }],
+      },
+    };
+
+    // Render the Home component with the mocked props
+    const { getByText, getAllByTestId } = render(<Home {...props} />);
+
+    // Test that no posts are rendered
+    expect(screen.queryAllByTestId('post').length).toBe(0);
+
+    // Test the rendering of banner images
+    const bannerImages = getAllByTestId('bannerImg');
+    expect(bannerImages.length).toBe(props.bannerImages.length);
+
+    // Test the rendering of categories and contact in the header
+    props.headerProps.categories.forEach((category) => {
+      expect(screen.getAllByText(category.subtitle).length).toBeGreaterThan(0);
+    });
+    props.headerProps.contact.forEach((contact) => {
+      expect(screen.getAllByText(contact.subtitle).length).toBeGreaterThan(0);
+    });
+
+    // Test the rendering of home text in the banner bottom
+    props.bannerBottomProps.homeText.forEach((homeText) => {
+      expect(getByText(homeText.title)).toBeInTheDocument();
+      expect(getByText(homeText.subtitle)).toBeInTheDocument();
+    });
+  });
 });
