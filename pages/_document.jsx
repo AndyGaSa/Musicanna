4b7424c08e90c66d6ce87e9396da74c4db18d7d8
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2023-08-16 01:52:24 +0200

    add lang to document

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,15 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
+export default function Document({ locale }) {
   return (
-    <Html>
+    <Html lang={locale || 'ca'}>
       <Head>
+        {/* Preconnect to Google Fonts for a tiny performance boost */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+
+        {/* Limit the number of font weights you're using for better performance */}
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Poppins:wght@400;500;600&display=swap"
           rel="stylesheet"
-        ></link>
+        />
+
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#fff" />
+
+        {/* Great! SEO-friendly description */}
         <meta
           name="description"
           content="Descobreix un món de contes i cançons per a nens i nenes en català. Aquesta web t’ofereix històries divertides, educatives i originals, acompanyades de música i il·lustracions. Ideal per estimular la imaginació, l’aprenentatge i l’amor per la llengua."
